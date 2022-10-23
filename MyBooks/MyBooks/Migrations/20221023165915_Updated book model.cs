f28d7b9509ccb6534c2a05b65f3e23011e5572e6
Suman Chava <suman.kumar257@gmail.com>
2022-10-23 22:37:26 +0530

    Added Books Model and DbContext

---
+++ MyBooks/MyBooks/Migrations/20221023165915_Updated book model.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBooks.Migrations
+{
+    public partial class Updatedbookmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CoverPageUrl",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateAdded",
+                table: "Books",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoverPageUrl",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "DateAdded",
+                table: "Books");
+        }
+    }
+}
--- MyBooks/MyBooks/Program.cs
+++ MyBooks/MyBooks/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MyBooks.Data;
+
 namespace MyBooks
 {
     public class Program
@@ -12,6 +15,7 @@ namespace MyBooks
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
             var app = builder.Build();
 
